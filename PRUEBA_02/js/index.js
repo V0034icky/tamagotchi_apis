d67f8ca638c2168d50370f2f388d6341a35fab86
V0034icky <166989600+V0034icky@users.noreply.github.com>
2025-11-04 22:19:38 -0700

    [UPDATE] cambios en js y css

--- PRUEBA_02/js/index.js
+++ PRUEBA_02/js/index.js
@@ -5,38 +5,40 @@ plantilla_tarjeta.remove();
 fetch("http://localhost:3000/")
     .then(respuesta => respuesta.json())
     .then(archivo => {
-
-        // Accede al arreglo dentro del JSON
         const lista_tamagotchis = archivo.resultados || archivo.arreglo || [];
 
         for (let i = 0; i < lista_tamagotchis.length; i++) {
-
-            var clon = plantilla_tarjeta.cloneNode(true);
+            const clon = plantilla_tarjeta.cloneNode(true);
             ColeccionTamagotchis.appendChild(clon);
 
+            const tamagotchi = lista_tamagotchis[i];
+
             const nombre = clon.querySelector(".nombre-tamagotchi");
-            nombre.innerHTML = lista_tamagotchis[i].nombre || "Desconocido";
+            nombre.innerHTML = tamagotchi.nombre || "Desconocido";
 
             const imagen = clon.querySelector(".img-tamagotchi");
-            if (lista_tamagotchis[i].imagen && lista_tamagotchis[i].imagen !== "") {
-                imagen.src = "http://localhost:3000/" + lista_tamagotchis[i].imagen;
+            let rutaImagen = Array.isArray(tamagotchi.imagen) ? tamagotchi.imagen[0] : tamagotchi.imagen;
+
+            if (rutaImagen && rutaImagen.startsWith("http")) {
+                imagen.src = rutaImagen;
+            } else if (rutaImagen && rutaImagen !== "") {
+                imagen.src = "http://localhost:3000/" + rutaImagen;
             } else {
                 imagen.src = "img/PLACEHOLDER.png";
             }
 
             const especie = clon.querySelector(".especie");
-            especie.innerHTML = `<strong>Especie:</strong><br>${lista_tamagotchis[i].especie || "Standard"}`;
+            especie.innerHTML = `<strong>Especie:</strong><br>${tamagotchi.especie || "Standard"}`;
 
             const generacion = clon.querySelector(".gen");
-            generacion.innerHTML = `<strong>${lista_tamagotchis[i].generacion || "?"}</strong>`;
+            generacion.innerHTML = `<strong>${tamagotchi.generacion || "?"}</strong>`;
 
             const anio = clon.querySelector(".anio");
-            anio.innerHTML = `<strong>Año:</strong> ${lista_tamagotchis[i].año || "Desconocido"}`;
+            anio.innerHTML = `<strong>Año:</strong> ${tamagotchi.año || "Desconocido"}`;
 
             const edicion = clon.querySelector(".edicion");
-            edicion.innerHTML = `<strong>Edición:</strong> ${lista_tamagotchis[i].edicion || "Base"}`;
+            edicion.innerHTML = `<strong>Edición:</strong> ${tamagotchi.edicion || "Base"}`;
         }
-
     })
     .catch(error => {
         console.error("Error al obtener datos:", error);
